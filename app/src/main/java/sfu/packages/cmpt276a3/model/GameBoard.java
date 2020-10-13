c5ed6c38c9291c68e3ef1866a0ebeeeb661e2aca
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-10-13 16:43:54 -0700

    Add scanner to check if mines present in row and column of button

--- app/src/main/java/sfu/packages/cmpt276a3/model/GameBoard.java
+++ app/src/main/java/sfu/packages/cmpt276a3/model/GameBoard.java
@@ -2,12 +2,10 @@ package sfu.packages.cmpt276a3.model;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,20 +13,29 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
 
+import java.util.Random;
+
 import sfu.packages.cmpt276a3.R;
 
 public class GameBoard extends AppCompatActivity {
 
     private static final int NUM_ROWS = 4;
-    private static final int NUM_COLS = 15;
+    private static final int NUM_COLS = 7;
+    private static final int NUM_MINES = 8;
 
     Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
+
+    // Array for location of mines, if spot contains hidden mine, int is 1,
+    // if contains revealed mine, int is 2, if spot already clicked once, int is 3, else int is 0
+    int mines[][] = new int[NUM_ROWS][NUM_COLS];
+    int nearbyHiddenMines[][] = new int[NUM_ROWS][NUM_COLS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game_board);
         populateBoard();
+        setMines();
     }
 
     private void populateBoard() {
@@ -49,14 +56,12 @@ public class GameBoard extends AppCompatActivity {
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f));
 
-                button.setText("" + col + "," + row);
-
                 // Make text not clip for small buttons
                 button.setPadding(0, 0, 0, 0);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        boardButtonClicked(FINAL_COL, FINAL_ROW);
+                        boardButtonClicked(FINAL_ROW, FINAL_COL);
                     }
                 });
 
@@ -66,24 +71,117 @@ public class GameBoard extends AppCompatActivity {
         }
     }
 
-    private void boardButtonClicked(int col, int row) {
-        Toast.makeText(this, "Button Clicked: " + col + "," + row, Toast.LENGTH_SHORT).show();
+    private void setMines() {
+        int timesRun = NUM_MINES;
+        for (int i = 0; i < timesRun; i++) {
+            Random rand = new Random();
+            int resultRow = rand.nextInt(NUM_ROWS);
+            int resultCol = rand.nextInt(NUM_COLS);
+
+            if (mines[resultRow][resultCol] == 1) {
+                timesRun++;
+            }
+            else {
+                mines[resultRow][resultCol] = 1;
+            }
+        }
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                if (mines[row][col] != 1) {
+                    mines[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    private void boardButtonClicked(int row, int col) {
         Button button = buttons[row][col];
 
         // Locks button sizes
         lockButtonSizes();
 
-        // Scale image to background
-        int newWidth = button.getWidth();
-        int newHeight = button.getHeight();
-        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fort);
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
-        Resources resource = getResources();
-        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+        if (mines[row][col] == 1) {
+            // Set revealed value to mine
+            mines[row][col] = 2;
 
-        // Change text on button
-        button.setText("" + col);
+            // Scale image to background
+            int newWidth = button.getWidth();
+            int newHeight = button.getHeight();
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fort);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+            Resources resource = getResources();
+            button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+        }
+        else if (mines[row][col] == 4) {
+            mines[row][col] = 3;
+        }
+        else {
+            mines[row][col] = 3;
+            scanBoard(row, col);
+        }
+        updateNearbyHiddenMines();
+    }
 
+    private void scanBoard(int mineRow, int mineCol) {
+        Button button = buttons[mineRow][mineCol];
+        int mineCount = 0;
+        //boolean mineFound = false;
+
+        for (int boardRow = 0; boardRow < NUM_ROWS; boardRow++) {
+            for (int boardCol = 0; boardCol < NUM_COLS; boardCol++) {
+
+               if (mineRow == boardRow) {
+                    if (mines[boardRow][boardCol] == 1) {
+                        mineCount++;
+                    }
+                }
+
+                if (mineCol == boardCol) {
+                    if (mines[boardRow][boardCol] == 1) {
+                        mineCount++;
+                    }
+                }
+            }
+        }
+        nearbyHiddenMines[mineRow][mineCol] = mineCount;
+        button.setText("" + nearbyHiddenMines[mineRow][mineCol]);
+        mineCount = 0;
+    }
+
+    private void updateNearbyHiddenMines() {
+        Button button;
+        int mineCount = 0;
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                if (mines[row][col] == 3) {
+                    for (int boardRow = 0; boardRow < NUM_ROWS; boardRow++) {
+                        for (int boardCol = 0; boardCol < NUM_COLS; boardCol++) {
+
+                            if (row == boardRow) {
+                                if (mines[boardRow][boardCol] == 1) {
+                                    mineCount++;
+                                }
+                            }
+
+                            if (col == boardCol) {
+                                if (mines[boardRow][boardCol] == 1) {
+                                    mineCount++;
+                                }
+                            }
+                        }
+                    }
+                    nearbyHiddenMines[row][col] = mineCount;
+                    button = buttons[row][col];
+                    button.setText("" + nearbyHiddenMines[row][col]);
+                    mineCount = 0;
+                }
+                if (mines[row][col] == 2) {
+                    mines[row][col] = 4;
+                }
+            }
+        }
     }
 
     private void lockButtonSizes() {
